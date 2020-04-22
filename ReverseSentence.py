ebb08589710a3e76cbf19e84521b289a3554e56f
brinda sahoo <brindasahoo.it@gmail.com>
2020-04-22 13:16:02 +0530

    Reverse a sentence

---
+++ ReverseSentence.py
@@ -0,0 +1,11 @@
+#Python program to reverse a sentence
+
+#Sample Input
+'''I am new to python programming'''
+#Sample Output
+'''Reversed sentence is : programming python to new am I'''
+str1 = str(input("Enter a sentence:"))
+word = str1.split()
+output = word[-1::-1]
+str2 = " ".join(output)
+print ("Reversed sentence is :",str2)
