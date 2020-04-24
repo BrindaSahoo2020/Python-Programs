aca571608d3875ad46fe4c0f0d71b56d05b6695b
brinda sahoo <brindasahoo.it@gmail.com>
2020-04-24 17:17:29 +0530

    To print the duplicates from a list of integers

---
+++ DuplicateNumber.py
@@ -0,0 +1,17 @@
+#Python program to print the duplicates from a list of integers
+
+#Sample Input
+'''L1 = [10,10,75, 21,-1,-4,-5,-1,-5,4,45, 4]'''
+#Sample Output
+'''List of duplicates are : [10, -1, -5, 4]'''
+
+L1 = [10,10,75, 21,-1,-4,-5,-1,-5,4,45, 4]
+l = len(L1)
+L2 = []
+for i in range(l) :
+    for j in range(i+1,l):
+        if L1[i] == L1[j] and L1[i] not in L2:
+            L2.append(L1[i])
+print("List of duplicates are :",L2)
+
+
