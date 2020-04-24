83fa19598158a7c856954d2ecaf7642b519ae56f
brinda sahoo <brindasahoo.it@gmail.com>
2020-04-24 12:30:50 +0530

    Program to remove multiple elements from a list using slice operation

---
+++ Slicing_RemoveElement.py
@@ -0,0 +1,10 @@
+#Python program to remove multiple elements from a list using slice operation
+
+#Sample Input
+'''Remove 1st to 5th elemnt'''
+#Sample Output
+'''After removal of elements from index 1 to 5 : [10, 23, 45, 67, 22, 20, 4, 45]'''
+
+L1 = [10, 21,-1,-4,-5,-722,23,45,67,22,20, 4, 45]
+del L1[1:6]
+print("After removal of elements from index 1 to 5 :",L1)
