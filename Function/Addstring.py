9712ca49ff5b919e4b534d0dc03770f1f8ce8a02
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-03 11:55:48 +0530

    Add two strings

---
+++ Function/Addstring.py
@@ -0,0 +1,24 @@
+#Add two strings
+
+#Sample Input
+'''n1 = "245"
+n2 = "1245"
+'''
+#Sample Out1put
+#1490
+
+def mysolution(n1,n2):
+    sum1 = int(n1) + int(n2)
+    print (sum1)
+    return sum1
+x1 = "245"
+x2 = "1245"
+mysolution(x1,x2)
+
+
+#Example 2
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
