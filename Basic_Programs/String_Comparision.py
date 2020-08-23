0031611433c20e7713415c4e83e391a37c933108
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-23 09:40:56 +0530

    String comparision basics

---
+++ Basic_Programs/String_Comparision.py
@@ -0,0 +1,20 @@
+#String comparision basics
+
+word = 'pytho'
+if (word == 'python'):
+    print("Everything is ok")
+elif(word < 'python'):
+    print("Letters are lesser than python")
+else:
+    print("Letters are more than python")
+
+#Output (Letters are lesser than python)
+'''
+Notes:
+==: This checks whether two strings are equal
+!=: This checks if two strings are not equal
+<: This checks if the string on its left is smaller than that on its right
+<=: This checks if the string on its left is smaller than or equal to that on its right
+>: This checks if the string on its left is greater than that on its right
+>=: This checks if the string on its left is greater than or equal to that on its right
+'''
