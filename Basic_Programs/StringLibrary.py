101d29d03c58de817bea0d122197d6f7dcb15fe3
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-23 11:37:51 +0530

    Basic examples  of String Library

---
+++ Basic_Programs/StringLibrary.py
@@ -0,0 +1,35 @@
+#Basic examples  of String Library
+
+str1 = 'Brinda Sahoo'
+str2 = str1.lower()
+print(str2)
+str3 = 'BRINDA'
+print(str3.lower())
+print('HI,My Name Is Brinda'.lower())
+
+#Output(brinda sahoo / brinda / hi,my name is brinda)
+
+#Syntax(center taes atleast one argument)
+#str1.center(width))
+print(str1.center(20))
+#Output    
+   #Brinda Sahoo 
+
+#Import String Libraries(Try out yourself.Many more are there)
+'''
+capitalize()	Converts the first character to upper case
+center()	Returns a centered string
+Returns the number of times a specified value occurs in a string
+index()	Searches the string for a specified value and returns the position of where it was found
+isalnum()	Returns True if all characters in the string are alphanumeric
+isalpha()	Returns True if all characters in the string are in the alphabet
+isdecimal()	Returns True if all characters in the string are decimals
+isdigit()	Returns True if all characters in the string are digits
+isidentifier()	Returns True if the string is an identifier
+islower()	Returns True if all characters in the string are lower case
+isnumeric()	Returns True if all characters in the string are numeric
+isupper()	Returns True if all characters in the string are upper case
+join()	Joins the elements of an iterable to the end of the string
+lower()	Converts a string into lower case
+lstrip()	Returns a left trim version of the string
+'''
