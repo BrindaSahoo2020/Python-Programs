c05b2e6c0a1b7b173ed4c87393297b151853c5f2
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-24 11:53:18 +0530

    Exercise on split() function

---
+++ Split.py
@@ -0,0 +1,17 @@
+#Exercise on split() function
+
+str1 = "brinda is learning python slowly"
+str2 = str1.split()
+str3 = str1.split(',')
+print(str2)
+print(str3)
+str4 = "from step3 extract 12.34.56.5"
+str5 = str4.split()
+print(str5[1])
+
+#Output
+'''
+['brinda', 'is', 'learning', 'python', 'slowly']
+['brinda is learning python slowly']
+step3
+'''
