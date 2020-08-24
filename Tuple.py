f946e7c130a2727c8a2aa97af4287817c0c668f0
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-24 20:31:32 +0530

    Exercises on Tuple

---
+++ Tuple.py
@@ -0,0 +1,24 @@
+#Programs on Tuple
+
+#Tuple Assignment
+
+(x,y) = (90,23)
+print(y)
+(m,n) = (80,'brinda')
+print(n)
+
+#Tuples comparision
+print ((0,1,2) < (1,4,6) )
+print ((0,1,2) < (0,1,3) )
+print ((0,1,2) < (0,1,2) )
+print (('aaa','bbb') < ('aab','aaa') )
+
+#Output
+'''
+23
+brinda
+True
+True
+False
+True
+'''
