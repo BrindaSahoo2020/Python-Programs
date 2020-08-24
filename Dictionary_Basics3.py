7ba93b5a70b16139cc6b8a7275e1f5fc4fd4a59c
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-24 19:36:43 +0530

    Two Iteration Variable in Dictionary

---
+++ Dictionary_Basics3.py
@@ -0,0 +1,14 @@
+#Two Iteration Variable in Dictionary
+
+dict1 =	{"School":"NHPS","Year":2020,"Class":4,"Name":"Aashna","RollNo":2}
+for x,y in dict1.items():
+    print(x,y)
+
+#Output
+'''
+School NHPS
+Year 2020
+Class 4
+Name Aashna
+RollNo 2
+'''
