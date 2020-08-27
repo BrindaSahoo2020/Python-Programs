599e49c48af6e5bc63bd51a31a560a2c459ee04d
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-27 17:25:55 +0530

    Python program to print ASCII Value of a character and string

--- ASCIIValue.py
+++ ASCIIValue.py
@@ -1,10 +1,18 @@
-#Python program to print ASCII Value of a character
+#Python program to print ASCII Value of a character and string
 
 #Sample Input
-''' c '''
+''' Enter any character :c '''
 #Sample Output
-'''ASCII value is 99'''
+'''
+ASCII value is  99
+ASCII values of the strings are : [98, 114, 105, 110, 100, 97]
+'''
 
-str1 = str(input("Enter a character : "))
+ch = str(input("Enter any character :"))
+x = ord(ch)
+print("ASCII value is " ,x)
+str1 = "brinda"
+lst1 = []
 for i in str1:
-    print("ASCII value is",ord(i))
+	lst1.append(ord(i))
+print("ASCII values of the strings are :", lst1)
