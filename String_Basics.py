172abfa42920ef74fb1f5002e69830fe3ec3e4e3
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-29 21:34:25 +0530

    Exercises on String

---
+++ String_Basics.py
@@ -0,0 +1,26 @@
+#Exercises on String Basics
+
+print('for'.isidentifier())
+print('abc'.islower())
+print('a@ 1,'.islower())
+print('11'.isnumeric())
+print('1.1'.isnumeric())
+print('1@ a'.isprintable())
+print(''''''.isspace())
+print('\t'.isspace())
+print('HelloWorld'.istitle())
+print('Hello World'.istitle())
+
+#Output
+'''
+True
+True
+True
+True
+False
+True
+False
+True
+False
+True
+'''
