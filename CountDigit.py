1a4057ad245c2e27581d60587a80575861e7b7b1
brinda sahoo <brindasahoo.it@gmail.com>
2020-08-31 16:43:22 +0530

    program to count the number of digits in a number

--- CountDigit.py
+++ CountDigit.py
@@ -3,7 +3,7 @@
 #Sample Input
 #Enter number:1678950
 #Sample Output
-#he number of digits in the number are: 7
+#The number of digits in the number are: 7
 
 n = int(input("Enter number:"))
 count = 0
@@ -11,3 +11,14 @@ while(n > 0):
     count = count+1
     n = n//10
 print("The number of digits in the number are:",count)
+
+#Method2
+
+n = int(input("Enter number:"))
+n1 = str(n)
+count = 0
+l = len(n1)
+for i in range(l):
+	count = count + 1
+print("The number of digits in the number are:",count)
+	
