627ccfe7c193a3d0b02bd08e5beaef4aa3fb10b9
brinda sahoo <brindasahoo.it@gmail.com>
2020-09-02 08:53:23 +0530

    Examples on Python basic math Module

---
+++ Math.py
@@ -0,0 +1,29 @@
+#Examples on Python basic math Module
+
+import math
+print(math.ceil(3.4))
+print(math.floor(3.4))
+print(math.copysign(3, -1))
+print(math.fabs(-3.4))
+print(math.factorial(6))
+print(math.factorial(4.0))
+print(math.trunc(3.1))
+print(math.exp(3))
+print(int(math.pow(3, 2)))
+print((math.pow(3, 2)))
+print(math.sqrt(4))
+
+#Output
+'''
+4
+3
+-3.0
+3.4
+720
+24
+3
+20.085536923187668
+9
+9.0
+2.0
+'''
