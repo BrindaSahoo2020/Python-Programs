7a95800b43f0b488e9344afb132bba8a3d996c0c
Brinda <brindasahoo2gmail.com>
2020-09-04 17:19:17 +0530

    Example on usage of *arg

---
+++ args.py
@@ -0,0 +1,29 @@
+#Example on usage of *arg
+
+def func(*argv):
+    for i in argv:
+        print(i)
+
+func('I','am','new','to','python')
+
+def mathematics(*arg):
+    for x in arg:
+        print(x)
+
+mathematics(1,2,3,4,5)
+
+def func1(arg1,*arg):
+    print("First argument is :",arg1)
+    for i in arg:
+        print(i)  
+
+func1('ab','cd','ef','gh')
+
+def add(*num):
+    sum1 = 0 
+    for i in num:
+        sum1 = sum1 + i
+    print("Sum:",sum1)
+
+add(3,5)
+add(4,5,6,7,9,0)
