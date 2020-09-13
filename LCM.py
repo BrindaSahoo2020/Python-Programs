087fbab16e319fb1021905148a13615b6766c387
Brinda <brindasahoo2gmail.com>
2020-09-13 08:39:26 +0530

    Calclate LCM of any numbers

--- LCM.py
+++ LCM.py
@@ -1,16 +1,13 @@
-#Calculate lcm of 2 numbers
+#Calculate lcm of any numbers
 
 x = int(input("Enter 1st number : "))
 y = int(input("Enter 2nd number : "))
+z = int(input("Enter 3rd number : "))
+max1 = max(x,y,z)
 
-if x > y:
-    z = x
-else:
-    z = y
 while(True):
-    if((z % x == 0) and (z % y == 0)):
-        lcm = z
+    if((max1 % x == 0) and (max1 % y == 0) and (max1 % z == 0)):
+        lcm = max1
         break
-    z += 1
-print(lcm)
-
+    max1 += 1
+print("LCM is :",lcm)
