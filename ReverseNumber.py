7e809ebdb7d95ee85c2340a4a3a46046bbcc7a2d
Brinda <brindasahoo2gmail.com>
2020-09-18 08:15:21 +0530

    Updated with an alternate method of finding the reverse of a number

--- ReverseNumber.py
+++ ReverseNumber.py
@@ -12,3 +12,15 @@ while(n > 0):
     rev = rev * 10 + dig
     n = n//10
 print("Reverse of the number:",rev)
+
+#Alternate method
+
+n = int(input("Enter number: "))
+str1 = str(n)
+str2 = ""
+for i in str1:
+	str2 = i + str2
+
+m = int(str2)
+print("Reverse of the number:",m)
+	
